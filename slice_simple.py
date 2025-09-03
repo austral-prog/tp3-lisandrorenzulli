d1d68fade63399742115f95f73564e4fe50f5f7d
lisandrorenzulli <lisandrorenzulli@gmail.com>
2025-09-03 17:06:37 -0300

    Add files via upload

--- slice_simple.py
+++ slice_simple.py
@@ -1,13 +1,7 @@
 def slice_simple():
     texto = "Awesome"
-    texto = texto.lower()
-
-    # Primeras 3 letras
-    print(texto[0:3])
-
-    # 3 letras del medio
+    texto = texto.lower()    
+    print(texto[0:3]) 
     print(texto[2:5])
-
-    # De la primera a la cuarta + de la antepenúltima a la última
     print(texto[0:4] + texto[-3:])
 slice_simple()
