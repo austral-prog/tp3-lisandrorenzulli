e848331d3245e5c46e78d36fd5475e561d800038
lisandrorenzulli <lisandrorenzulli@gmail.com>
2025-09-03 17:08:44 -0300

    Add files via upload

--- in_string.py
+++ in_string.py
@@ -1,9 +1,7 @@
 def check_vowels():
-    
     texto = input("Ingrese un nombre: ")
     texto = texto.lower()
     vocales = ["a", "e", "i", "o", "u"]
-
     for vocal in vocales:
         print(f"Contiene {vocal}: {vocal in texto}")
 check_vowels()
